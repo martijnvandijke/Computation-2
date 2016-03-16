aa11294a6f10100d76d46dc1ca04403568e7fb1f
Martyn van Dijke <m.v.dijke@student.tue.nl>
2016-03-16 19:48:29 +0100

    second update

--- Report/calculation.m
+++ Report/calculation.m
@@ -1,21 +1,21 @@
      %Ref - clipping - lcc - forwarding - division
 Cycles = [2350122, 2315926, 2108766,  496593 ];
-Freq = [56.625,56.625 ,56.626,56.387 ];
+Freq = [56.625,56.625 ,56.626, 56.387 ];
 Freq = Freq .* 10^6;
 
 T = Cycles ./ Freq ;
 
-G = (T - T) ./ T; 
+%G = (T - T) ./ T; 
 
-for i = 1 : 4
-         if(i <4  )
+for i = 1 : size(T)
+         %if(i <4  )
 %             i= i+1
 %         else
 %             b = i 
-            G(i) = ( T(1 ) - T(i)  ) ./ ( T(1) ) .* 100;
+            G(i) = ( 0.041503258278146 - T(i)  ) ./ ( 0.041503258278146 ) .* 100;
             
             
-        end
+        %end
       
 end
 %G = T
